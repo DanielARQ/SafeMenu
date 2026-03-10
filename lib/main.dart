cfde9d39c3f66f251ace31a8102f0a819227ff5b
DanielARQ <Systemw2a@gmail.com>
2026-03-09 19:37:27 -0600

    Correcion main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:safemenu/view/welcome_view.dart';
-
 import 'firebase_options.dart';
-
 import 'package:safemenu/service/user_prefs.dart';
-
-import 'package:safemenu/view/splash_view.dart';
-import 'package:safemenu/view/onboarding_view.dart';
 import 'package:safemenu/view/login_view.dart';
 import 'package:safemenu/view/register_view.dart';
 import 'package:safemenu/view/main_wrapper.dart';
@@ -63,31 +58,3 @@ class SafeMenuApp extends StatelessWidget {
   }
 }
 
-/**
- * const SafeMenuApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SafeMenu',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF4CAF50)),
-        useMaterial3: true,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => SplashView(),
-        '/onboarding': (context) => const OnboardingView(),
-        '/login': (context) => const LoginView(),
-        '/register': (context) => const RegisterView(),
-        '/setup': (context) => const AllergySetupView(),
-        '/main_wrapper': (context) => const MainWrapper(), // <--- ESTA LÍNEA ES LA QUE FALTA
-        '/home': (context) => const MainWrapper(),
-        '/camera': (context) => const CameraView(),
-        '/results': (context) => const ScanResultsView(),
-        '/detail': (context) => const DetailView(),
-      },
-    );
-  }
- */
